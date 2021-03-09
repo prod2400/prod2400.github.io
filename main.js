786320e57402b1e85589f2df81da19e05722319c
prod2400 <prod24@protonmail.com>
2021-03-09 13:01:09 -0500

    4
    4

---
+++ main.js
@@ -0,0 +1,38 @@
+let info = `My names Pedro Rodrigues,I\'m a senior at 
+Bullard Havens Technical
+ High School
+and after high school I would like to pursure a career in 
+Electrical Engeering.`;
+
+
+const lpBOX = document.getElementById("landing-page")
+
+
+
+let test = ()=>{
+    console.log("hi");
+    sleep(1);
+    console.log("bye");
+}
+
+test();
+
+function sleep(secs){
+    let d = new Date();
+    let start = d.getSeconds();
+    let end = start + secs;
+    console.log(start);
+    while (true){
+        let e = new Date().getSeconds()
+        if (e >= end){
+            break;
+        }
+    }
+}
+
+i = 0
+while (i < info.length){
+    lpBOX.textContent += info[i];
+    sleep(1);
+    i++
+}
